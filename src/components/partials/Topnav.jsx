987637914c4a90c49c27f472b686ba3fa37abeb7
Aaron Mathew <aaronmathew453@gmail.com>
2024-07-02 01:02:18 +0530

    Top navigation search bar working

--- src/components/partials/Topnav.jsx
+++ src/components/partials/Topnav.jsx
@@ -1,32 +1,36 @@
 import axios from '../../utils/axios';
-import React, { useEffect, useState } from 'react'
-import { Link } from 'react-router-dom'
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+import noimage from "/noimage.jpg";
 
 const Topnav = () => {
     const [query, setquery] = useState("");
-    const GetSerches = async() => {
+    const [searches, setsearches] = useState([]);
+    const GetSearches = async () => {
       try {
-        const d = await axios.get(`/search/multi?query=${query}`);
-        console.log(d);
+        const { data } = await axios.get(`/search/multi?query=${query}`);
+        setsearches(data.results);
       } catch (error) {
         console.log("Error: ", error);
       }
     };
     
     useEffect(() => {
-      GetSerches();
-    },[query]);
+      GetSearches();
+    }, [query]);
   
   return (
     <div className='w-full h-[10vh] relative flex justify-start items-center ml-[15%]'>
-        <i class="text-zinc-400 text-3xl ri-search-line"></i>
+        <i className="text-zinc-400 text-3xl ri-search-line"></i>
         <input onChange={(e) => setquery(e.target.value)} value={query} className='w-[50%] text-zinc-200 mx-10 p-5 text-xl outline-none border-none bg-transparent' type="text" placeholder='search anything' />
-        {query.length > 0 && (<i onClick={() => setquery("")} class="text-zinc-400 text-3xl ri-close-fill"></i>)}
+        {query.length > 0 && (<i onClick={() => setquery("")} className="text-zinc-400 text-3xl ri-close-fill"></i>)}
         <div className='absolute w-[50%] max-h-[50vh] bg-zinc-200 top-[90%] overflow-auto'>
-            {/* <Link className='hover:text-black hover:bg-zinc-300 duration-300 font-semibold text-zinc-600 w-[100%] p-10 flex justify-start items-center border-b-2 border-zinc-100'>
-                <img src="" alt="" />
-                <span>Hello Everyone</span>
-            </Link> */}
+            {searches.map((s,i) => (
+              <Link key={i} className='hover:text-black hover:bg-zinc-300 duration-300 font-semibold text-zinc-600 w-[100%] p-10 flex justify-start items-center border-b-2 border-zinc-100'>
+                <img className='w-[10v] h-[10vh] object-cover rounded mr-5 shadow-lg' src={s.backdrop_path || s.profile_path ? `https://image.tmdb.org/t/p/original/${s.backdrop_path || s.profile_path}` : noimage} alt="" />
+                <span>{s.name || s.title || s.original_name || s.original_title}</span>
+            </Link>
+          ))}
         </div>
     </div>
   )
@@ -34,4 +38,4 @@ const Topnav = () => {
 
 export default Topnav
 
-// { 1:02:41 } 1st Video
+// { 1:14:23 } 1st Video
