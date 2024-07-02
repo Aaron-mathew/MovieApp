8b7e3a6d34027b00f558e911e1e67c207caff30b
Aaron Mathew <aaronmathew453@gmail.com>
2024-07-02 23:32:48 +0530

    Made the trending now walpaper

--- src/components/partials/Topnav.jsx
+++ src/components/partials/Topnav.jsx
@@ -20,11 +20,11 @@ const Topnav = () => {
     }, [query]);
   
   return (
-    <div className='w-full h-[10vh] relative flex justify-start items-center ml-[15%]'>
+    <div className='w-[80%] h-[10vh] relative flex mx-auto items-center'>
         <i className="text-zinc-400 text-3xl ri-search-line"></i>
         <input onChange={(e) => setquery(e.target.value)} value={query} className='w-[50%] text-zinc-200 mx-10 p-5 text-xl outline-none border-none bg-transparent' type="text" placeholder='search anything' />
-        {query.length > 0 && (<i onClick={() => setquery("")} className="text-zinc-400 text-3xl ri-close-fill"></i>)}
-        <div className='absolute w-[50%] max-h-[50vh] bg-zinc-200 top-[90%] overflow-auto'>
+        {query.length > 0 && (<i onClick={() => setquery("")} className="text-zinc-400 text-3xl ri-close-fill right-0"></i>)}
+        <div className='absolute w-[50%] max-h-[50vh] bg-zinc-200 top-[100%] left-[5%] overflow-auto'>
             {searches.map((s,i) => (
               <Link key={i} className='hover:text-black hover:bg-zinc-300 duration-300 font-semibold text-zinc-600 w-[100%] p-10 flex justify-start items-center border-b-2 border-zinc-100'>
                 <img className='w-[10v] h-[10vh] object-cover rounded mr-5 shadow-lg' src={s.backdrop_path || s.profile_path ? `https://image.tmdb.org/t/p/original/${s.backdrop_path || s.profile_path}` : noimage} alt="" />
@@ -38,4 +38,3 @@ const Topnav = () => {
 
 export default Topnav
 
-// { 1:14:23 } 1st Video
