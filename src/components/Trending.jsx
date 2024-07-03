1c53db09010a90f418928b4d2c18ae8bde612533
Aaron Mathew <aaronmathew453@gmail.com>
2024-07-04 00:40:11 +0530

    Added infinite scrolling to trending page

--- src/components/Trending.jsx
+++ src/components/Trending.jsx
@@ -1,33 +1,52 @@
-import React, { useEffect, useState } from 'react'
+import { useEffect, useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 import Topnav from './partials/Topnav';
 import Dropdown from './partials/Dropdown';
 import axios from '../utils/axios';
 import Cards from './partials/Cards';
 import Loading from './Loading';
+import InfiniteScroll from 'react-infinite-scroll-component';
 
 const Trending = () => {
     const navigate = useNavigate();
     const [category, setcategory] = useState("all");
     const [duration, setduration] = useState("day");
-    const [trending, settrending] = useState(null);
+    const [trending, settrending] = useState([]);
+    const [page, setpage] = useState(1);
+    const [hasMore, sethasMore] = useState(true);
     
     const GetTrending = async () => {
         try {
-          const { data } = await axios.get(`/trending/${category}/${duration}`);
-          settrending(data.results);
-        } catch (error) {
-          console.log("Error: ", error);
-        }
+            const { data } = await axios.get(`/trending/${category}/${duration}?page=${page}`);
+            if(data.results.length > 0){
+                settrending((prevState) => [...prevState, ...data.results]);
+                setpage(page+1);
+            } else {
+                sethasMore(false);
+            }   
+            
+            } catch (error) {
+            console.log("Error: ", error);
+            }
       };
-      console.log(trending)
+      
+      const refreshHandler = () => {
+        if(trending.length ===  0){
+            GetTrending();
+        } else {
+            setpage(1);
+            settrending([]);
+            GetTrending();
+        }
+      }
+
       useEffect(() => {
-        GetTrending();
+        refreshHandler();
       },[category, duration])
 
-  return trending ? (
-    <div className='px-[3%] w-screen h-screen overflow-hidden overflow-y-auto'>
-        <div className='w-full flex items-center justify-between'>
+  return trending.length > 0 ? (
+    <div className='w-screen h-screen'>
+        <div className='px-[5%] w-full flex items-center justify-between'>
             <h1 className='text-2xl font-semibold text-zinc-400 '><i onClick={() => navigate(-1)} className="hover:text-[#6556CD] ri-arrow-left-line"></i>  Trending</h1>
             <div className='flex items-center w-[80%]'>
                 <Topnav />
@@ -36,9 +55,13 @@ const Trending = () => {
                 <Dropdown title="Durartion" options={["week", 'day']} func={(e) => setduration(e.target.value)} />
             </div>
         </div>
-        <Cards data={trending} title={category} />
+        <InfiniteScroll dataLength={trending.length} next={GetTrending} hasMore={hasMore} loader={<h1>Loading...</h1>}>
+            <Cards data={trending} title={category} />
+        </InfiniteScroll>
     </div>
   ): <Loading />
 }
 
 export default Trending
+
+// {52:10} 2nd Video
