0368ae992f4bc3f5d64a45aa23f8099b364e105f
Aaron Mathew <aaronmathew453@gmail.com>
2024-07-08 23:21:07 +0530

    Added some more stuff in details page

--- src/components/MovieDetails.jsx
+++ src/components/MovieDetails.jsx
@@ -1,10 +1,12 @@
 import React, { useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux';
 import { asyncloadmovie, removemovie } from '../store/actions/movieActions';
-import { Link, useNavigate, useParams } from 'react-router-dom';
+import { Link, useLocation, useNavigate, useParams } from 'react-router-dom';
+import HorizontalCards from './partials/HorizontalCards';
 import Loading from './Loading';
 
 const MovieDetails = () => {
+  const { pathname } = useLocation();
   const navigate = useNavigate();
   const { id } = useParams();
   const { info } =useSelector((state) => state.movie);
@@ -16,14 +18,14 @@ const MovieDetails = () => {
       dispatch(removemovie())
     };
 
-  }, []);
+  }, [id]);
   return info ? (
     <div style={{
       background: `linear-gradient(rgba(0,0,0,.2),rgba(0,0,0,.5),rgba(0,0,0,.8)), url(https://image.tmdb.org/t/p/original/${info.detail.backdrop_path })`, 
       backgroundPosition: "center",
       backgroundSize: "cover",
       backgroundRepeat: "no-repeat",
-                }}  className='w-screen h-screen px-[10%]'>
+                }}  className='w-screen h-[150vh] px-[10%]'>
       {/* Part 1 navigation */}
       <nav className='h-[10vh] w-full text-zinc-100 flex items-center gap-10 text-xl'>
         <Link onClick={() => navigate(-1)} className="hover:text-[#6556CD] ri-arrow-left-line"></Link> 
@@ -31,14 +33,35 @@ const MovieDetails = () => {
         <a target="_blank"  href={`https://www.wikidata.org/wiki/${info.externalid.wikidata_id}`}><i className ="ri-earth-fill"></i></a>
         <a target="_blank"  href={`https://www.imdb.com/title/${info.externalid.imdb_id}/`}>imdb</a>
       </nav>
+
       {/* Part 2 Poster and details */}
       <div className='w-full flex'>
-                <img className='shadow-[8px_17px_38px_2px_rgba(0,0,0,.5)] h-[40vh] object-cover' src={`https://image.tmdb.org/t/p/original/${ info.detail.poster_path || info.detail.backdrop_path }`} alt="" />
-                <div className='content ml-[5%]'>
-                  <h1 className='text-5xl font-black text-white'>
+                <img className='shadow-[8px_17px_38px_2px_rgba(0,0,0,.5)] h-[50vh] object-cover' src={`https://image.tmdb.org/t/p/original/${ info.detail.poster_path || info.detail.backdrop_path }`} alt="" />
+                <div className='content ml-[5%] text-white'>
+                  <h1 className='text-5xl font-black'>
                     {info.detail.name || info.detail.title || info.detail.original_name || info.detail.original_title}
                     <small className='text-2xl font-bold text-zinc-200'>({info.detail.release_date.split("-")[0]})</small>
                   </h1>
+
+                  <div className='mt-3 mb-5 flex items-center gap-x-5 '>
+                    <span className='rounded-full text-xl font-semibold bg-yellow-600 text-white w-[5vh] h-[5vh] flex justify-center items-center'>
+                            {(info.detail.vote_average*10).toFixed()}<sup>%</sup>
+                    </span>
+                    <h1 className='w-[60px] font-semibold text-2xl leading-6'>User Score</h1>
+                    <h1>{info.detail.release_date}</h1>
+                    <h1>{info.detail.genres.map((g) => g.name).join(",")}</h1>
+                    <h1>{info.detail.runtime}min</h1>
+                  </div>
+
+                  <h1 className='text-xl font-semibold italic text-zinc-200'>{info.detail.tagline}</h1>
+                  
+                  <h1 className='text-2xl mb-3 mt-5'>Overview</h1>
+                  <p>{info.detail.overview}</p>
+
+                  <h1 className='text-2xl mb-3 mt-5'>Movie Translated</h1>
+                  <p className='mb-10'>{info.translations.join(", ")}</p>
+
+                  <Link className='p-5 bg-[#6556CD] rounded-lg' to={`${pathname}/trailer`}><i className="text-xl mr-3 ri-play-fill"></i>Play Trailer</Link>
                 </div>
       </div>
 
@@ -66,6 +89,13 @@ const MovieDetails = () => {
 
 
       </div>
+
+      {/* Part4 Recommendation and similar stuff */}
+      <hr className='mt-10 mb-5 border-none h-[2px] bg-zinc-400'/>
+      <h1 className=' text-3xl font-bold text-white'>Recommendations & Similar Stuff</h1>
+      <HorizontalCards data={
+        info.recommendations.length > 0 ? info.recommendations : info.similar} />
+
     </div>
   ): <Loading /> 
 }
@@ -73,4 +103,4 @@ const MovieDetails = () => {
 export default MovieDetails
 
 
-// {1:27:59} 3rd Video
+// {2:03:34} 3rd Video
