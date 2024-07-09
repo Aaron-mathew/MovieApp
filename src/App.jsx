902bd690c20b388ed31aaffffa7125d994f2443d
Aaron Mathew <aaronmathew453@gmail.com>
2024-07-10 00:05:48 +0530

    Made the not found page

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,7 @@ import MovieDetails from './components/MovieDetails'
 import TvDetails from './components/TvDetails'
 import PersonDetails from './components/PersonDetails'
 import Trailer from './components/partials/Trailer'
+import NotFound from './components/NotFound'
 
 const App = () => {
   return (
@@ -26,7 +27,7 @@ const App = () => {
           <Route path="/tv/details/:id" element={<TvDetails />} />
         <Route path="/person" element={<People />} />
           <Route path="/person/details/:id" element={<PersonDetails />} />
-
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </div>
   )
--- src/components/partials/HorizontalCards.jsx
+++ src/components/partials/HorizontalCards.jsx
@@ -6,7 +6,7 @@ const HorizontalCards = ({data}) => {
 
 
         <div className='w-[100%] flex overflow-y-hidden mb-5 p-5'>
-            {data.map((d,i) => (
+            {data.length > 0 ? data.map((d,i) => (
                 <Link to={`/${d.media_type}/details/${d.id}`} key={i} className='min-w-[15%] h-[35vh] bg-zinc-900 mr-5 mb-5'>
                 <img className='w-full h-[55%] object-cover ' src={`https://image.tmdb.org/t/p/original${d.backdrop_path || d.poster_path}`} alt="" />
                 <div className=' text-white p-3 h-[45%] overflow-y-auto'>
@@ -14,7 +14,7 @@ const HorizontalCards = ({data}) => {
                     <p className=''>{d.overview.slice(0, 50)}...<span className='text-zinc-500'>more</span></p>
                 </div>
             </Link>
-            ))}
+            )): <h1 className='text-3xl mt-5 text-white font-black text-center'>Nothing to show</h1> }
         </div>
 
   )
